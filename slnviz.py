d60fb62ffe0c3c00f577091672affb869d9579dd
Jostein Kjønigsen <jostein@kjonigsen.net>
2017-09-25 14:44:15 +0200

    Traverse project-files for declared dependencies.

--- slnviz.py
+++ slnviz.py
@@ -9,9 +9,20 @@
 from argparse import ArgumentParser
 import re
 import os
+import xml.etree.ElementTree as ET
+
+def get_unix_path(file):
+    return file.replace("\\", "/")
 
 
 solution_path = "."
+
+def set_working_basedir(sln_file):
+    global solution_path
+    solution_path = get_directory(get_unix_path(sln_file))
+    print("Base-solution dir set to {0}".format(solution_path))
+
+project_reference_declaration = re.compile("{(.*)}")
 
 class Project(object):
     def __init__(self, name, filename, id):
@@ -28,6 +39,46 @@ class Project(object):
 
     def add_dependency(self, id):
         self.dependant_ids.append(id)
+
+    def get_full_project_file_path(self):
+        return os.path.join(solution_path, get_unix_path(self.filename))
+
+    def get_project_references(self, xml_doc):
+        nodes = []
+        for elem in xml_doc.getiterator():
+            if "ProjectReference" in elem.tag:
+                nodes.append(elem)
+        return nodes
+
+    def get_project_ids(self, nodes):
+        result = []
+        for node in nodes:
+            for elem in node.getiterator():
+              if "Project" in elem.tag:
+                  match = project_reference_declaration.match(elem.text)
+                  if match:
+                      result.append(match.groups()[0].upper())
+        return result
+        
+    def get_declared_project_dependency_ids(self):
+        xml_proj = self.get_full_project_file_path()
+        print("--Project {0}-- Scanning dependencies...".format(self.name))
+        if not os.path.isfile(xml_proj):
+            print("--Project {0}-- Couldn't open project-file '{1}'".format(self.name, xml_proj))
+            return []
+        
+        xml_doc = ET.parse(xml_proj).getroot()
+        nodes = self.get_project_references(xml_doc)
+        ids = self.get_project_ids(nodes)
+        return ids
+
+    
+    def apply_declared_project_dependencies(self):
+        ids = self.get_declared_project_dependency_ids()
+        print("--Project {0}-- Found {1} dependencies in project file.".format(self.name, len(ids)))
+        for id in ids:
+            print("--Project {0}-- Adding dependency to project {1}.".format(self.name, id))
+            self.add_dependency(id)    
 
 
 def get_project_by_id(projects, id):
@@ -71,6 +122,10 @@ def analyze_projects_in_solution(lines):
             if id1 == id2:
                 current_project.add_dependency(id1)
 
+    # pull in dependencies declared in project-files
+    for project in projects:
+        project.apply_declared_project_dependencies()
+                
     return projects
 
 
@@ -94,8 +149,11 @@ def render_dot_file(projects):
         proj1_id = project.get_friendly_id()
         for id in project.dependant_ids:
             proj2 = get_project_by_id(projects, id)
-            proj2_id = proj2.get_friendly_id()
-            lines.append("    {0} -> {1}".format(proj1_id, proj2_id))
+            if proj2 is None:
+                print("WARNING: Unable to resolve dependency with ID {0} for project {1}".format(id, project.name))
+            else:
+              proj2_id = proj2.get_friendly_id()
+              lines.append("    {0} -> {1}".format(proj1_id, proj2_id))
     
     lines.append("")
     lines.append("}")
@@ -103,17 +161,9 @@ def render_dot_file(projects):
     return "\n".join(lines)
 
 
-def get_unix_path(file):
-    return file.replace("\\", "/")
-
-
 def get_directory(file):
     unix_file = get_unix_path(file)
     return os.path.split(unix_file)[0]
-
-
-def set_working_basedir(sln_file):
-    solution_path = get_directory(get_unix_path(sln_file))
 
 
 def process(sln_file, dot_file):
